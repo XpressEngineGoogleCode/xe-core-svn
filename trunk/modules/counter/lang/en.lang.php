da65cd770ee3dbd1efac77398fe90b86d29fbb17
zero <zero@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-09-07 06:07:04 +0000

    2007. 09. 07 ê°ë°ì¤ì¸ sandboxë¥¼ beta.0.1.3 releasesë¥¼ ìí´ trunkë¡ ì´ë r.2550

---
+++ trunk/modules/counter/lang/en.lang.php
@@ -0,0 +1,22 @@
+<?php
+    /**
+     * @file   modules/counter/lang/en.lang.php
+     * @author zero <zero@nzeo.com>
+     * @brief  English Language Pack (Only basic contents are listed)
+     **/
+
+    $lang->counter = "Counter";
+    $lang->cmd_select_date = 'Select date';
+    $lang->cmd_select_counter_type = array(
+        'hour' => 'By Hour',
+        'day' => 'By Day',
+        'month' => 'By Month',
+        'year' => 'By Year',
+    );
+
+    $lang->total_counter = 'Total Status';
+    $lang->selected_day_counter = 'Selected day status';
+
+    $lang->unique_visitor = 'Visitors';
+    $lang->pageview = 'Pageview';
+?>
