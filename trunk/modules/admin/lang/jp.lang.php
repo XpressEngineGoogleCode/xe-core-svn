708b681a0b419759a2e61eb4a63ee6204f378802
zero <zero@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2007-11-05 04:16:02 +0000

    0.2.4 버전 배포를 위해 sandbox를 trunk로 copy

---
+++ trunk/modules/admin/lang/jp.lang.php
@@ -0,0 +1,88 @@
+<?php
+    /**
+     * @file   jp.lang.php
+     * @author zero (zero@nzeo.com) 翻訳：RisaPapa
+     * @brief  日本語言語パッケージ（基本的な内容のみ）
+     **/
+
+    $lang->admin_info = '관리자 정보';
+    $lang->admin_index = '관리자 초기 페이지';
+
+    $lang->module_category_title = array(
+        'service' => '서비스형 모듈',
+        'manager' => '관리형 모듈',
+        'utility' => '기능성 모듈',
+        'accessory' => '부가 모듈',
+        'base' => '기본 모듈',
+    );
+
+    $lang->newest_news = "最新ニュース";
+
+    $lang->env_setup = "環境設定";
+
+    $lang->env_information = "환경 정보";
+    $lang->current_version = "설치된 버전";
+    $lang->current_path = "설치된 경로";
+    $lang->released_version = "최신 버전";
+    $lang->about_download_link = "설치된 버전보다 최신의 버전이 배포되었습니다. download링크를 클릭하시면 다운 받으실 수 있습니다.";
+
+    $lang->item_module = "モジュールリスト";
+    $lang->item_addon  = "アドオンリスト";
+    $lang->item_widget = "ウィジェットリスト";
+    $lang->item_layout = "レイアウトリスト";
+
+    $lang->module_name = "モジュール名";
+    $lang->addon_name = "アドオン名";
+    $lang->version = "バージョン";
+    $lang->author = "作者";
+    $lang->table_count = "テーブル数";
+    $lang->installed_path = "インストールパス";
+
+    $lang->cmd_shortcut_management = "メニュー編集";
+
+    $lang->msg_is_not_administrator = '管理者のみ接続できます';
+    $lang->msg_manage_module_cannot_delete = 'モジュール、アドオン、ウィジェットのショットカットは削除できません。';
+    $lang->msg_default_act_is_null = 'デフォルトの管理者のアクションが指定されていないため、ショットカットを登録することができません。';
+
+    $lang->welcome_to_zeroboard_xe = 'ゼロボードXEの管理者ページです。';
+    $lang->about_admin_page = "管理者ページはまだ未完成です。クローズベタバージョンの期間に、多くの方々からご意見をいただきながら、必ず必要なコンテンツを埋めていきたいと思います。";
+    $lang->about_lang_env = "위 설정한 언어셋을 처음 방문하는 사용자들에게 동일하게 적용하기 위해서는 원하는 언어로 변경후 아래 [저장] 버튼을 클릭하시면 됩니다";
+
+    $lang->zeroboard_xe_user_links = 'ユーザのためのリンク';
+    $lang->zeroboard_xe_developer_links = 'デベロッパーのためのリンク';
+
+    $lang->xe_user_links = array(
+        '公式ホームページ' => 'http://www.zeroboard.com',
+        //'クローズベタサイト' => 'http://spring.zeroboard.com',
+        //'モジュルダ情報' => 'http://www.zeroboard.com',
+        //'アドオン情報' => 'http://www.zeroboard.com',
+        //'ウィジェット情報' => 'http://www.zeroboard.com',
+        //'モジュール・スキン情報' => 'http://www.zeroboard.com',
+        //'ウィジェットスキン情報' => 'http://www.zeroboard.com',
+        //'レイアウトスキン情報' => 'http://www.zeroboard.com',
+    );
+
+    $lang->xe_developer_links = array(
+        'デベロッパーフォーラム' => 'http://spring.zeroboard.com',
+        //'マニュアル' => 'http://www.zeroboard.com/wiki/manual',
+        'イッシュートラッキング' => 'http://trac.zeroboard.com',
+        'SVN Repository' => 'http://svn.zeroboard.com',
+        'Doxygen Document' => 'http://doc.zeroboard.com',
+        'PDFドキュメント' => 'http://doc.zeroboard.com/zeroboard_xe.pdf',
+    );
+
+    $lang->zeroboard_xe_usefulness_module = '有用なモジュール';
+    $lang->xe_usefulness_modules = array(
+        'dispEditorAdminIndex' => 'エディター管理',
+        'dispDocumentAdminList' => 'ドキュメント管理',
+        'dispCommentAdminList' => 'コメント管理',
+        'dispFileAdminList' => '添付ファイル管理',
+        'dispPollAdminList' => 'アンケート管理',
+        'dispSpamfilterAdminConfig' => 'スパムフィルター管理',
+        'dispCounterAdminIndex' => 'カウンターログ',
+
+    );
+
+    $lang->xe_license = 'ゼロボードXEのライセンスはGPLです。';
+    $lang->about_shortcut = 'よく使用するモジュールに登録されたショットカットは削除できます。';
+?>
---
+++ trunk/modules/comment/lang/es.lang.php
@@ -0,0 +1,27 @@
+<?php
+    /**
+     * @archivo   modules/comment/lang/ko.lang.php
+     * @auhor zero <zero@nzeo.com>
+     * @sumario  Paquete del idioma español para el módulo de comentarios.
+     **/
+	
+    $lang->cmd_comment_do = '이 댓글을..';
+    
+    $lang->comment_list = '댓글 목록';
+    $lang->cmd_delete_checked_comment = 'Eliminar lo seleccionado' ;
+
+    $lang->msg_cart_is_null = 'Selecciona el commentario que desea eliminar';
+    $lang->msg_checked_comment_is_deleted = '%d comentario eliminado correctamente.';
+
+    $lang->search_target_list = array(
+        'content' => 'Contenido',
+        'user_id' => 'ID',
+        'user_name' => 'Nombre',
+        'nick_name' => 'Apodo',
+        'email_address' => 'Correo Electrónico',
+        'homepage' => 'Página web',
+        'regdate' => 'Fecha del registro',
+        'last_update' => 'Ultima actualización',
+        'ipaddress' => 'Dirección IP',
+    );
+?>
