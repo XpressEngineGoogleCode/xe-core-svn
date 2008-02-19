d90826dbe0085118b2f6d44e41df947168ff8bfa
zero <zero@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-02-19 16:28:53 +0000

    script path 찾는 함수를 일단 0.2.8로 rollback. 차후 보다 자세한 환경에서 테스트후 원위치

---
+++ trunk/modules/addon/lang/fr.lang.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * @file   modules/addon/lang/fr.lang.php
+     * @author zero (zero@nzeo.com)  Traduit par Pierre Duvent(PierreDuvent@gamil.com)
+     * @brief  Paquet de la Langue francaise
+     **/
+
+    $lang->addon = "Compagnon";
+
+    $lang->addon_info = 'Le résumé de la Compagnon';
+    $lang->addon_maker = 'L\'Auteur de la Compagnon';
+    $lang->addon_history = 'L\'Histoire de la Compagnon';
+
+    $lang->about_addon = 'La Compagnon contrôle les actions plutôt d\'imprimer des résultats de HTML.<br/>Seulement par touche à bascule des compagnons que vous voulez faire marcher ou arrêter, vous pouvez appliquer les fonctions très utiles pour administrer votre site web.';
+?>
+
+
