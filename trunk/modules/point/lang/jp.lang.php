39cf4f748e5533d4ad87234998b21c7a2cdb82c5
zero <zero@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2008-02-19 16:28:53 +0000

    script path 찾는 함수를 일단 0.2.8로 rollback. 차후 보다 자세한 환경에서 테스트후 원위치

---
+++ trunk/modules/point/lang/jp.lang.php
@@ -0,0 +1,62 @@
+<?php
+    /**
+     * @file   modules/point/lang/jp.lang.php
+     * @author zero <zero@nzeo.com> 翻訳：RisaPapa
+     * @brief  ポイント（point）モジュールの基本言語パッケージ
+     **/
+
+    $lang->point = "ポイント"; 
+    $lang->level = "レベル"; 
+
+    $lang->about_point_module = "ポイントモジュールでは、書き込み作成/コメント作成/アップロード/ダウンロードなどのユーザの活動に対してポイントの計算を行います。但し、ポイントモジュールでは設定のみを行い、アドオンでポイントシステムを「使用」に設定しなければポイントは累積されません。";
+    $lang->about_act_config = "掲示板、ブログなどのモジュールごと書き込み作成・削除/コメント作成・削除などのアクションがあります。掲示板/ブログ以外のモジュールにポイントシステムを連動させたい場合は、各機能のアクションの「act値」を追加します。連動は「,（コンマ）」で区切って追加します。";
+
+    $lang->max_level = '最高レベル';
+    $lang->about_max_level = '最高レベルを指定することができます。最高レベルは「1000」がマクシマムなので、レベルアイコンに注意が必要です。';
+
+    $lang->level_icon = 'レベルアイコン';
+    $lang->about_level_icon = 'レベルアイコンは、「./modules/point/icons/レベル.gif」で指定されるため、最高レベルとアイコンセットが異なる場合がありますので、注意してください。';
+
+    $lang->point_name = 'ポイント名';
+    $lang->about_point_name = 'ポイントの名前、単位が指定できます。';
+
+    $lang->level_point = 'レベルポイント';
+    $lang->about_level_point = '下の各レベルのポイントが増加したり、減少するとレベルが調整されます。';
+
+    $lang->disable_download = 'ダウンロード禁止';
+    $lang->about_disable_download = 'チェックするとポイントがない場合、ダウンロードを禁止します（イメージファイル除外）。';
+
+    $lang->level_point_calc = 'レベル別ポイント計算';
+    $lang->expression = 'レベル変数<b>i</b>を使用してJavaスクリプト数式を入力してください（例: Math.pow(i, 2) * 90）';
+    $lang->cmd_exp_calc = '計算';
+    $lang->cmd_exp_reset = '初期化';
+
+    $lang->cmd_point_recal = 'ポイント再計算';
+    $lang->about_cmd_point_recal = '掲示物/コメント/添付ファイル全てを調べ、ポイント設定に合わせて、全会員のポイントを再計算します。';
+
+    $lang->point_link_group = 'グループ連動';
+    $lang->about_point_link_group = 'グループにレベルを指定すると、該当レベルになったらグループが変更されます。 ただし、新しいグループに変更されると以前自動登録されたグループは消去されます。';
+
+    $lang->about_module_point = 'モジュール別にポイントを指定することができますが、指定されていないモジュールでは、デフォルトポイントが使用されます。すべてのポイント数は、反対のアクションを行った際には原状復帰されます。';
+
+    $lang->point_signup = '加入';
+    $lang->point_insert_document = '書き込み作成';
+    $lang->point_delete_document = '書き込み削除';
+    $lang->point_insert_comment = 'コメント作成';
+    $lang->point_delete_comment = 'コメント削除';
+    $lang->point_upload_file = 'アップロード';
+    $lang->point_delete_file = 'ファイル削除';
+    $lang->point_download_file = 'ダウンロード';
+    $lang->point_read_document = '書き込み照会';
+
+
+    $lang->cmd_point_config = 'デフォルト設定';
+    $lang->cmd_point_module_config = 'モジュール別設定';
+    $lang->cmd_point_act_config = '機能別アクション設定';
+    $lang->cmd_point_member_list = '会員ポイントリスト';
+
+    $lang->msg_cannot_download = 'ポイントが不足しているため、ダウンロードできません。';
+
+    $lang->point_recal_message = 'ただ今ポイントを適用しています。 (%d / %d)';
+    $lang->point_recal_finished = 'ポイント再計算が完了しました。';
+?>
