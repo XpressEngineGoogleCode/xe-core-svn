deec8885bc21bf1034a7ca5deef26a2fc04ca115
bnu <bnu@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-01-20 02:08:00 +0000

    code_highlighter 에디터 컴포넌트 추가

---
+++ faceOff/modules/editor/components/code_highlighter/lang/jp.lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author ミニミ
+ **/
+$lang->code_type = '言語種類';
+
+$lang->used_collapse = '折りたたみ機能を使う';
+$lang->hidden_linenumber = '行番号を隠す';
+$lang->hidden_controls = 'ツールバーを隠す';
+
+$lang->file_path = 'File Path';
+$lang->description = 'Description';
+$lang->first_line = 'First Line';
---
+++ faceOff/modules/editor/components/code_highlighter/lang/zh-CN.lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author haojilin
+ **/
+$lang->code_type = '语言类型';
+
+$lang->used_collapse = '使用代码折叠';
+$lang->hidden_linenumber = '隐藏行号';
+$lang->hidden_controls = '隐藏工具栏';
+
+$lang->file_path = 'File Path';
+$lang->description = 'Description';
+$lang->first_line = 'First Line';
