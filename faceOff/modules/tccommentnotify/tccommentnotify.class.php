6a49af62e7b6be2058d93f27429a0bf2c549bcad
zero <zero@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-01-28 06:16:12 +0000

    tccommentnotify action forward 오타 수정

--- faceOff/modules/tccommentnotify/tccommentnotify.class.php
+++ faceOff/modules/tccommentnotify/tccommentnotify.class.php
@@ -31,7 +31,7 @@
         function checkUpdate() {
             $oModuleModel = &getModel('module');
             if(!$oModuleModel->getTrigger('comment.insertComment', 'tccommentnotify', 'controller', 'triggerInsertComment', 'after')) return true;
-            if(!$oModuleModel->getActionForward('tccommentnotify','view','dispCommentNotfiyAdminIndex')) return true;
+            if(!$oModuleModel->getActionForward('tccommentnotify','view','dispCommentNotifyAdminIndex')) return true;
 
 
             return false;
@@ -44,7 +44,7 @@
             $oModuleModel = &getModel('module');
             $oModuleController = &getController('module');
             if(!$oModuleModel->getTrigger('comment.insertComment', 'tccommentnotify', 'controller', 'triggerInsertComment', 'after')) $oModuleController->insertTrigger('comment.insertComment', 'tccommentnotify', 'controller', 'triggerInsertComment', 'after');
-            if(!$oModuleModel->getActionForward('tccommentnotify','view','dispCommentNotfiyAdminIndex')) 
+            if(!$oModuleModel->getActionForward('tccommentnotify','view','dispCommentNotifyAdminIndex')) 
                 $oModuleController->insertActionForward('tccommentnotify', 'view', 'dispCommentNotifyAdminIndex');
 
             return new Object(0, 'success_updated');
