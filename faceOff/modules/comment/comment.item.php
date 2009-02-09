f1cf1d866dd7fc8b093e553556435909dc93b1ef
zero <zero@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-02-09 01:34:04 +0000

    댓글 알림 쪽지의 댓글 글자 자르기 길이는 10에서 30으로 늘림

--- faceOff/modules/comment/comment.item.php
+++ faceOff/modules/comment/comment.item.php
@@ -115,7 +115,7 @@
 
             // 변수 정리
             if($type) $title = "[".$type."] ";
-            $title .= cut_str(strip_tags($content), 10, '...');
+            $title .= cut_str(strip_tags($content), 30, '...');
             $content = sprintf('%s<br /><br />from : <a href="%s#comment_%s" onclick="window.open(this.href);return false;">%s</a>',$content, $oDocument->getPermanentUrl(), $this->get('comment_srl'), $oDocument->getPermanentUrl());
             $receiver_srl = $this->get('member_srl');
             $sender_member_srl = $logged_info->member_srl;
