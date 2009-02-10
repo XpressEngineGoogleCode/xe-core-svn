3cc6426b794d32b4a20924ec5b3629572dace67e
zero <zero@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-02-10 07:41:49 +0000

    카운터 애드온을 모듈 동작 이전 시점으로 변경 (정상 출력 프로세스를 거치지 않고 미리 종료하는 경우에도 수집하기 위함)

--- faceOff/addons/counter/counter.addon.php
+++ faceOff/addons/counter/counter.addon.php
@@ -6,9 +6,8 @@
      * @author zero (zero@nzeo.com)
      * @brief 카운터 애드온
      **/
-
     // called_position가 before_display_content 일 경우 실행
-    if(Context::isInstalled() && $called_position == 'before_display_content' && Context::get('module')!='admin') {
+    if(Context::isInstalled() && $called_position == 'before_module_init' && Context::get('module')!='admin') {
         $oCounterController = &getController('counter');
         $oCounterController->procCounterExecute();
     }
