b2b639b464a57ec8e98216e911c8db1426df1351
sol <sol@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-03-05 09:13:36 +0000

    fix language select widget position

--- faceOff/widgets/language_select/skins/default/js/language_select.js
+++ faceOff/widgets/language_select/skins/default/js/language_select.js
@@ -3,6 +3,9 @@ jQuery(function(){
         var c = jQuery(this).parent();
         var langList = c.next('ul.langList');
         langList.toggle();
+
+        jQuery(this).parents('[id^=ex_]:first').css('overflow','').css('position','relative');
+
         if(langList.css('top')){
             if(jQuery('body').height() <= c.offset().top + langList.height()) {
                 langList.css('top',-(langList.height()+17));
