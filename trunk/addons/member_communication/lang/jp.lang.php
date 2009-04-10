c0d448ecb8822a489f9313083f53b127a0120ca0
sol <sol@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-04-10 05:36:56 +0000

    for 1.2.1

--- trunk/addons/member_communication/lang/jp.lang.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $lang->alert_new_message_arrived = '%d個の新しいメッセージが届きました。確認しますか。';
-?>
--- trunk/layouts/cafeXE/lang/jp.lang.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $lang->new_members = '新会員のお知らせ';
-    $lang->document_ranking = 'ゲシゲウルランキング';
-    $lang->comment_ranking = 'コメント数ランキング';
-?>
