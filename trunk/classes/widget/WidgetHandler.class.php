2264df3871eb229368640d7d37808310963d6c90
haneul0318 <haneul0318@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-11-25 10:42:58 +0000

    copy 1.3.1 to trunk

---
+++ trunk/classes/widget/WidgetHandler.class.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * @class WidgetHandler
+     * @author zero (zero@nzeo.com)
+     * @brief 위젯의 실행을 담당
+     **/
+
+    class WidgetHandler {
+
+        var $widget_path = '';
+
+    }
+?>
