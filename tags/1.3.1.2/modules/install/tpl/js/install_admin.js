d77b86fbfd3fd9481aae11081e004aefcb93109e
haneul0318 <haneul0318@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2009-12-21 10:04:43 +0000

    add tags

---
+++ tags/1.3.1.2/modules/install/tpl/js/install_admin.js
@@ -0,0 +1,37 @@
+/**
+ * @brief 설치 완료후 실행될 함수
+ */
+function completeInstalled(ret_obj) {
+    alert(ret_obj["message"]);
+    location.href = "./index.php?module=admin";
+}
+
+/**
+ * @brief FTP 정보 입력
+ **/
+function doInstallFTPInfo(fo_obj) {
+    var oFilter = new XmlJsFilter(fo_obj, "install", "procInstallFTP", completeInstallFTPInfo);
+    oFilter.addResponseItem("error");
+    oFilter.addResponseItem("message");
+    return oFilter.proc();
+}
+
+function completeInstallFTPInfo(ret_obj) {
+    location.href = current_url;
+}
+
+function doCheckFTPInfo() {
+    var fo_obj = jQuery("#ftp_form").get(0);
+    var oFilter = new XmlJsFilter(fo_obj, "install", "procInstallCheckFTP", completeInstallCheckFtpInfo);
+    oFilter.addResponseItem("error");
+    oFilter.addResponseItem("message");
+    return oFilter.proc();
+}
+
+function completeInstallCheckFtpInfo(ret_obj) {
+    alert(ret_obj['message']);
+}
+
+function completeFtpPath(ret_obj){
+   location.reload(); 
+}
