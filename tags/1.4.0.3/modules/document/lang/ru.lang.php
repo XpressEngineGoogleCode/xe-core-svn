462113b8fd7ed06d21d181dbf41112250eafd3e4
haneul0318 <haneul0318@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2010-01-28 08:37:55 +0000

    tag 1.4.0.3

---
+++ tags/1.4.0.3/modules/document/lang/ru.lang.php
@@ -0,0 +1,76 @@
+<?php
+  /**
+     * @file   ru.lang.php
+     * @author zero <zero@nzeo.com> | translation by Maslennikov Evgeny aka X-[Vr]bL1s5 | e-mail: x-bliss[a]tut.by; ICQ: 225035467;
+     * @brief  Russian basic language pack
+     **/
+
+    $lang->document_list = 'Список документов';
+    $lang->thumbnail_type = 'Тип миниатюры';
+    $lang->thumbnail_crop = 'Обрезать';
+    $lang->thumbnail_ratio = 'Отношение';
+    $lang->cmd_delete_all_thumbnail = 'Удалить все миниарюры';
+    $lang->move_target_module = "Переместить в";
+    $lang->title_bold = 'Жирное название';
+    $lang->title_color = 'Цвет названия';
+    $lang->new_document_count = '새글';
+
+    $lang->parent_category_title = 'Родительская категория';
+    $lang->category_title = 'Категория';
+    $lang->category_color = '분류 폰트색깔';
+    $lang->expand = 'Расширить';
+    $lang->category_group_srls = 'Доступные группы';
+    $lang->about_category_title = 'Пожалуйста, введите название категории';
+    $lang->about_expand = 'Если эта опция выбрана, расширение будут применено всегда';
+    $lang->about_category_group_srls = '선택하신 그룹만 현재 카테고리를 지정할 수 있도록 합니다';
+
+    $lang->about_category_color = '분류 폰트색깔을 지정합니다.';
+    $lang->cmd_temp_save = 'Сохранить временно';
+
+	$lang->cmd_toggle_checked_document = 'Перевернуть выбранные объекты';
+    $lang->cmd_delete_checked_document = 'Удалить выбранные';
+    $lang->cmd_document_do = 'Действия';
+
+    $lang->msg_cart_is_null = 'Выберите статьи, которые Вы хотите удалить';
+    $lang->msg_category_not_moved = 'Невозможно переместить';
+    $lang->msg_is_secret = 'Эта статья секретна';
+    $lang->msg_checked_document_is_deleted = 'Всего %d статей было удалено';
+
+    // Search targets in admin page
+        $lang->search_target_list = array(
+        'title' => 'Тема',
+        'content' => 'Содержание',
+        'user_id' => 'Юзер ID',
+        'member_srl' => 'Членский No.',
+        'user_name' => 'Имя',
+        'nick_name' => 'Ник',
+        'email_address' => 'Email',
+        'homepage' => 'Домашняя страница',
+        'is_notice' => 'Уведомление',
+        'is_secret' => 'Секрет',
+        'tags' => 'Тег',
+        'readed_count' => 'Кол-во Просмотров (Выше)',
+        'voted_count' => 'Кол-во Голосов (Выше)',
+        'comment_count ' => 'Кол-во Комментариев (Выше)',
+        'trackback_count ' => 'Кол-во Трекбеков (Выше)',
+        'uploaded_count ' => 'Кол-во Вложений (Выше)',
+        'regdate' => 'Дата',
+        'last_update' => 'Последнее Обновление',
+        'ipaddress' => 'IP-Адрес',
+    );
+    $lang->alias = "Alias";
+    $lang->history = "History";
+    $lang->about_use_history = "Determine whether to enable history feature, if it is enabled, update history would be stored and possible to restore old revisions.";
+    $lang->trace_only = "Trace only";
+
+    $lang->cmd_trash = 'Trashcan';
+    $lang->cmd_restore = 'Restore';
+    $lang->cmd_restore_all = 'Restore All';
+
+    $lang->in_trash = 'Trashcan';
+    $lang->trash_nick_name = 'Person who deleted';
+    $lang->trash_date = 'Deleted date';
+    $lang->trash_description = 'Description';
+
+    $lang->success_trashed = "Successfully moved to trashcan";
+?>
