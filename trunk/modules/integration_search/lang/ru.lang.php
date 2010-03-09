865c8bcbe31a5423e56e376005ce4d9dd8c60b82
haneul0318 <haneul0318@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2010-03-09 09:54:35 +0000

    copy trunk, tags

---
+++ trunk/modules/integration_search/lang/ru.lang.php
@@ -0,0 +1,42 @@
+<?php
+  /**
+     * @file   ru.lang.php
+     * @author zero <zero@nzeo.com> | translation by Maslennikov Evgeny aka X-[Vr]bL1s5 | e-mail: x-bliss[a]tut.by; ICQ: 225035467;
+     * @brief  Russian basic language pack
+     **/
+
+    $lang->integration_search = "Интеграция поиска";
+
+    $lang->sample_code = "Код примера";
+    $lang->about_target_module = "Только выбранные модули являются назначением. Пожалуйста, будте осторожны, устанавливая контроль";
+    $lang->about_sample_code = "Вы можете добавить интеграцию поиска на лейаут посредством добавления выше указанного кода";
+    $lang->msg_no_keyword = "Введите ключевое слово для поиска";
+
+    $lang->is_result_text = "<strong>%d</strong> результатов для <strong>'%s'</strong>";
+    $lang->multimedia = "Images/ Movies";
+    
+    $lang->include_search_target = 'Лишь в отдельных целевых';
+    $lang->exclude_search_target = 'Поиск на выбранный пункт назначения из';
+
+    $lang->is_search_option = array(
+        'document' => array(
+            'title_content' => 'Тема+Содержание',
+            'title' => 'Тема',
+            'content' => 'Содержание',
+            'tag' => 'Теги',
+        ),
+        'trackback' => array(
+            'url' => 'URL назначения',
+            'blog_name' => 'Имя сайта',
+            'title' => 'Заголовок',
+            'excerpt' => 'Выдержка',
+        ),
+    );
+
+    $lang->is_sort_option = array(
+        'regdate' => 'Дата регистрации',
+        'comment_count' => 'Кол-во комментариев',
+        'readed_count' => 'Кол-во хитов',
+        'voted_count' => 'Кол-во голосов',
+    );
+?>
