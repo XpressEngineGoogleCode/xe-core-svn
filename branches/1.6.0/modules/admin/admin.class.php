e8ad0ab6d1058fd9037ce49caa87ac1087dd921d
dionisrom@gmail.com <dionisrom@gmail.com@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2012-03-29 16:25:49 +0000

    Fix for issue #1798.

--- branches/1.6.0/modules/admin/admin.class.php
+++ branches/1.6.0/modules/admin/admin.class.php
@@ -286,7 +286,7 @@ class admin extends ModuleObject {
                 break;
             case 'adminConfigurationGeneral':
             case 'adminConfigurationFtp':
-            case 'adminMenuSetup':
+            //case 'adminMenuSetup':
             case 'fileUpload':
             case 'filebox':
             case 'spamFilter':
