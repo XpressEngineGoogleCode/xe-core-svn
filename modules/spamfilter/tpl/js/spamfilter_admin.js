9d7c5679e4ac379c26afbca29ccb92afa77a4fae
dionisrom <dionisrom@gmail.com>
2012-04-04 12:11:58 +0000

    Fix for issueFix for issue #1845.

--- modules/spamfilter/tpl/js/spamfilter_admin.js
+++ modules/spamfilter/tpl/js/spamfilter_admin.js
@@ -19,28 +19,4 @@ function doDeleteDeniedWord(word) {
     fo_obj.ruleset.value = 'deleteDeniedWord';
     fo_obj.submit();
 }
-function doInsertDeniedIP(msg_invalid_format){
-    var fo_obj = get_by_id('spamfilterInsert');
-    var reg_ipaddress = /^(\d{1,3}(?:.(\d{1,3}|\*)){3}\s*(\/\/[^\r\n]*)?[\r\n]*)*$/;
-    var matchStr_ipaddress = fo_obj.ipaddressList.value;
-    if(!matchStr_ipaddress.match(reg_ipaddress)) {
-        alert(msg_invalid_format);
-        return false;
-    }
-    fo_obj.act.value = "procSpamfilterAdminInsertDeniedIP";
-    fo_obj.ruleset.value = "insertDeniedIp";
-    fo_obj.submit();
-}
-function doInsertDeniedWord(msg_invalid_format){
-    var fo_obj = get_by_id('spamfilterInsert');
-    var reg_word = /^(.{2,40}[\r\n]+)*.{0,40}$/;
-    var matchStr_word = fo_obj.wordList.value;
-    if(!matchStr_word.match(reg_word)) {
-        alert(msg_invalid_format);
-        return false;
-    }
-    fo_obj.act.value = "procSpamfilterAdminInsertDeniedWord";
-    fo_obj.ruleset.value = "insertDeniedWord";
-    fo_obj.submit();
-}
 
