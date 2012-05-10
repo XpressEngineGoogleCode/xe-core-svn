37a1b87481428813d9e9180a270080355240b913
sinsy200 <sinsy200@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2012-05-10 09:50:05 +0000

    merge driver features from 1.6.0.member classes: 10017,10068,10072-10073 modules/install: 10017-10072 modules/module: 10017,10068,10099,10669

--- branches/1.6.0.driver/classes/db/DBMssql.class.php
+++ branches/1.6.0.driver/classes/db/DBMssql.class.php
@@ -346,7 +346,7 @@
          * @brief Create a table by using xml file
          **/
         function createTableByXmlFile($file_name) {
-            if(!file_exists($file_name)) return;
+            if(!file_exists($file_name)) return FALSE;
             // read xml file
             $buff = FileHandler::readFile($file_name);
             return $this->_createTable($buff);
@@ -365,7 +365,7 @@
             $xml_obj = $oXml->parse($xml_doc);
             // Create a table schema
             $table_name = $xml_obj->table->attrs->name;
-            if($this->isTableExists($table_name)) return;
+            if($this->isTableExists($table_name)) return FALSE;
 
             if($table_name == 'sequence') {
                 $table_name = $this->prefix.$table_name;
---
+++ branches/1.6.0.driver/classes/driver/Driver.php
@@ -0,0 +1,190 @@
+<?php
+
+/**
+ * @brief Super class of Driver
+ * @developer NHN (Developers@xpressengine.com)
+ */
+abstract class Driver extends Object
+{
+	private $moduleName = NULL;
+	private $modulePath = NULL;
+	private $driverName = NULL;
+	private $driverPath = NULL;
+	private $driverTplPath = NULL;
+	private $driverInfo = NULL;
+
+	/**
+	 * @brief Set module name
+	 * @access public
+	 * @param $moduleName name of module
+	 * @return void
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	public function setModuleName($moduleName)
+	{
+		$this->moduleName = $moduleName;
+		$this->computeModulePath();
+		$this->computeDriverPath();
+		$this->setDriverInfo();
+	}
+
+	/**
+	 * @brief Set module path
+	 * @access private
+	 * @return void
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	private function computeModulePath()
+	{
+		if(!isset($this->moduleName))
+		{
+			$this->modulePath = NULL;
+			return;
+		}
+
+		$this->modulePath = ModuleHandler::getModulePath($this->moduleName);
+	}
+
+	/**
+	 * @brief Get module name
+	 * @access public
+	 * @return string
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	public function getModuleName()
+	{
+		return $this->moduleName;
+	}
+
+	/**
+	 * @brief Get module path
+	 * @access public
+	 * @return string
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	public function getModulePath()
+	{
+		return $this->modulePath;
+	}
+
+	/**
+	 * @brief Set driver name
+	 * @access public
+	 * @param $driverName name of driver
+	 * @return void
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	public function setDriverName($driverName)
+	{
+		$this->driverName = $driverName;
+		$this->computeDriverPath();
+		$this->setDriverInfo();
+	}
+
+	/**
+	 * @brief Set driver name
+	 * @access private
+	 * @param $driverName name of driver
+	 * @return void
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	private function setDriverInfo()
+	{
+		if(!isset($this->moduleName, $this->driverName))
+		{
+			$this->driverInfo = NULL;
+		}
+
+		$oModuleModel = getModel('module');
+		$this->driverInfo = $oModuleModel->getDriverInfoXml($this->moduleName, $this->driverName);
+	}
+
+	/**
+	 * @brief Get driver info
+	 * @access public
+	 * @return stdClass
+	 * @developer NHN (devlopers@xpressengine.com)
+	 */
+	public function getDriverInfo()
+	{
+		return $this->driverInfo;
+	}
+
+	/**
+	 * @brief Get driver name
+	 * @access public
+	 * @return string
+	 * @developer NHN (devlopers@xpressengine.com)
+	 */
+	public function getDriverName()
+	{
+		return $this->driverName;
+	}
+
+	/**
+	 * @brief Compute driver's path
+	 * @access private
+	 * @return void
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	private function computeDriverPath()
+	{
+		if(!isset($this->moduleName, $this->driverName))
+		{
+			$this->driverPath = NULL;
+			return;
+		}
+
+		$this->driverPath = sprintf('%sdrivers/%s/', $this->modulePath, $this->driverName);
+		$this->driverTplPath = sprintf('%sdrivers/%s/tpl/', $this->modulePath, $this->driverName);
+	}
+
+	/**
+	 * @brief Get driver path
+	 * @access public
+	 * @return string
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	public function getDriverPath()
+	{
+		return $this->driverPath;
+	}
+
+	/**
+	 * @brief Get driver template path
+	 * @access public
+	 * @return string
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	public function getDriverTplPath()
+	{
+		return $this->driverTplPath;
+	}
+
+	/**
+	 * @brief Install Driver (Child must override this method)
+	 * @access public
+	 * @return boolean
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	abstract public function installDriver();
+
+	/**
+	 * @brief Check update (Child must override this method)
+	 * @access public
+	 * @return boolean
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	abstract public function checkUpdate();
+
+	/**
+	 * @brief Update Driver (Child must override this method)
+	 * @access public
+	 * @return Object
+	 * @developer NHN (developers@xpressengine.com)
+	 */
+	abstract public function updateDriver();
+}
+
+/* End of file Driver.php */
+/* Location: ./classes/driver/Driver.php */
