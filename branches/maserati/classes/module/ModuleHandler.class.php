519f0f16a8723209e97120eac2af24bf7be3c07d
sinsy200@gmail.com <sinsy200@gmail.com@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2013-03-07 09:45:22 +0000

    issue 3115. fixed a bug that displays a link for mobile on preive of editor

--- branches/maserati/classes/module/ModuleHandler.class.php
+++ branches/maserati/classes/module/ModuleHandler.class.php
@@ -566,7 +566,11 @@ class ModuleHandler extends Handler
 		$this->module_info->module_type = $type;
 		$oModule->setModuleInfo($this->module_info, $xml_info);
 
-		if($type == "view" && $this->module_info->use_mobile == "Y" && Mobile::isMobileCheckByAgent() && Context::get('act') != 'dispLayoutPreviewWithModule')
+		$skipAct = array(
+				'dispEditorConfigPreview' => 1,
+				'dispLayoutPreviewWithModule' => 1
+		);
+		if($type == "view" && $this->module_info->use_mobile == "Y" && Mobile::isMobileCheckByAgent() && !isset($skipAct[Context::get('act')]))
 		{
 			global $lang;
 			$header = '<style>div.xe_mobile{opacity:0.7;margin:1em 0;padding:.5em;background:#333;border:1px solid #666;border-left:0;border-right:0}p.xe_mobile{text-align:center;margin:1em 0}a.xe_mobile{color:#ff0;font-weight:bold;font-size:24px}@media only screen and (min-width:500px){a.xe_mobile{font-size:15px}}</style>';
