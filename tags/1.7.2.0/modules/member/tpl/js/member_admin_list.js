1bc45266a8b78dc05c41c4aa52ea579312acf795
ovclas@gmail.com <ovclas@gmail.com@201d5d3c-b55e-5fd7-737f-ddc643e51545>
2013-03-12 04:45:34 +0000

    add tagging for 1.7.2.0

---
+++ tags/1.7.2.0/modules/member/tpl/js/member_admin_list.js
@@ -0,0 +1,19 @@
+jQuery(function($){
+	$('a.modalAnchor._member').bind('before-open.mw', function(){
+		var $memberList = $('#memberList tbody :checked');
+		if ($memberList.length == 0){
+			alert(xe.lang.msg_select_user);
+			return false;
+		}
+		var memberInfo, memberSrl;
+		var memberTag = "";
+		$('input[name="groups[]"]:checked').removeAttr('checked');
+		$('#message').val('');
+		for (var i = 0; i<$memberList.length; i++){
+			memberInfo = $memberList.eq(i).val().split('\t');
+			memberSrl = memberInfo.shift();
+			memberTag += '<tr><td>'+memberInfo.join("</td><td>")+'<input type="hidden" name="member_srls[]" value="'+memberSrl+'"/></td></tr>' 
+		}
+		$('#popupBody').empty().html(memberTag);
+	});
+});
